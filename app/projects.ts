ad75a4891473045d72a001ffa2d9b19b4e1b92cc
Luke D <luke.warmwater6@gmail.com>
2023-08-21 23:32:33 -0400

    Projects page is semi-formatted

--- app/projects.ts
+++ app/projects.ts
@@ -20,7 +20,9 @@ export const projects: Project[] = [
     tech: ["Next.js", "TypeScript", "TailwindCSS"],
     url: "https://luke-davison.vercel.app/",
     sourceUrl: "https://github.com/luke-davison/portfolio",
-    notes: []
+    notes: [
+      "One day I woke up and thought to myself, \"It's about time I had a proper portfolio site\".  Also, \"It's about time I tried Next.js\".  I managed to whip this up in a couple of days, converting a template I found on Red Pixel Themes to React components.  I like to think that I have an eye for detail, but I've known for a while that I'm no designer.  I also had to do some minor tweaking - so if you see something that doesn't quite look right, that'll likely be my changes to the design."
+    ]
   },
   // {
   //   slug: "countdown-solver",
@@ -46,7 +48,9 @@ export const projects: Project[] = [
     url: "https://luke-and-betty-blog.netlify.app/",
     sourceUrl: "https://github.com/luke-davison/gatsby-blog",
     notes: [
-      "My family has a tradition of writing travel blogs so that everyone back home knows what they get up to.  I don't care that much to follow tradition, but I quickly realise that a blog was a perfect way to immortalise holiday memories.  Originally I wrote them using free wordpress.com sites, but I decided that I wanted a solution where I could keep them all in one place (without having to pay wordpress) and also ensure they are backed up so they can't be lost."
+      "My family has a tradition of writing travel blogs so that everyone back home knows what they get up to.  I didn't plan to follow tradition, but I quickly realised that a blog was a perfect way to immortalise holiday memories.  It's like a photo album that also tells a story.  I don't care if no one else reads them - these blog entries are mostly for me.",
+      "Originally I wrote them using free wordpress.com sites, but I decided that I wanted a solution where I could keep them all in one place (without having to pay wordpress) and also ensure they are backed up so they can't be lost.  Github probably isn't the best place to store images, but it works :).",
+      "One problem I had with the blog is that it would sometimes have problems rendering the images.  I never did get to the bottom of it but it sounded like maybe I was pushing what Gatsby was intended for (there's now a lot of blog posts).  I found that reducing the size of the image (rather than relying on Gatsby to do this) seemed to solve the problem.  It wasn't ideal - but I was a week away from flying out at the time so it had to do."
     ]
   },
   {
@@ -65,14 +69,14 @@ export const projects: Project[] = [
   {
     slug: "word-game",
     image: "/portfolio-word-game.PNG",
-    title: "Unnamed Word Game",
+    title: "Lettermonger",
     date: "2022",
     description: "A word game not too similar or dissimilar from Wordle",
     tech: ["React", "TypeScript", "Node", "Express", "CSS"],
     url: "https://lettermonger.onrender.com",
     sourceUrl: "https://github.com/luke-davison/new-word-game",
     notes: [
-      "In 2019 I was inspired by the success of Wordle to create my own daily word game.  It is similar to a single turn in scrabble, except each tile scores differently based on factors such as where it is in the word, what other letters are nearby etc.  I was quite happy with how fun the game ended up being, but I was not able to find a way to make it look good on a phone in portrait orientation."
+      "In 2019 I was inspired by the success of Wordle to create my own daily word game.  It is similar to a single turn in scrabble, except each tile scores differently based on factors such as where it is in the word, what other letters are nearby etc.  I was quite happy with how fun the game ended up being, but I was not able to find a way to make it look good enough on a phone in portrait orientation."
     ]
   },
   {
@@ -105,17 +109,22 @@ export const projects: Project[] = [
     tech: ["JavaScript"],
     url: "https://luke-davison.github.io/city-sim/",
     sourceUrl: "https://github.com/luke-davison/city-sim",
-    notes: []
+    notes: [
+      "Living temporarily in Auckland I was fascinated and infuriated with the traffic.  I decided to put my new learnings to use and make a simulation of traffic jams.  Ultimately that vision was warped once I found an isometric sprite set and decided to make a little city instead.",
+      "There's not much to this - just watch the cars drive round and round.  Also I wouldn't dare look at the code.  This was written before I kicked the self-taught-coder habits that made my code awful to read.  I know better now."
+    ]
   },
   {
     slug: "maze-drawer",
     image: "/portfolio-maze-drawer.png",
     title: "Maze drawer",
     date: "August 2017",
-    description: "Simple widget that draws a maze",
+    description: "A simple widget that draws a maze",
     tech: ["JavaScript", "HTML Canvas"],
     url: "https://luke-davison.github.io/maze-drawer/",
     sourceUrl: "https://github.com/luke-davison/maze-drawer",
-    notes: []
+    notes: [
+      "Like the city sim, this one doesn't do a lot.  It just draw's a pretty maze-like design on the screen.  During the bootcamp we were assigned to make something and I just felt like making a maze that day."
+    ]
   }
 ];
--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -13,42 +13,54 @@ export default function ProjectPage({ params }: { params: { slug: string } }) {
     : [project.sourceUrl];
 
   return (
-    <div>
-      <h2>{project.title}</h2>
-      <p>Date: {project.date}</p>
-      <h4>{project.description}</h4>
-      <Image
-        src={`/assets/img${project.image}`}
-        className="w-full shadow"
-        alt="portfolio image"
-        width={552}
-        height={294}
-        priority
-      />
-      {project.notes.map((note, index) => (
-        <p key={index}>{note}</p>
-      ))}
-      <p>Tech used: {project.tech.join(", ")}</p>
-      <p className="flex">
-        <span>Live site:&nbsp;</span>
-        <span>
-          {url.map((str) => (
-            <a key={str} href={str} className="block">
-              {str}
-            </a>
+    <div className="container flex flex-col items-center py-16 md:py-20 lg:flex-row">
+      <div className="w-full sm:w-3/4 lg:w-3/5 text-center m-auto">
+        <h2 className="font-header text-4xl font-semibold uppercase text-primary sm:text-5xl lg:text-6xl">
+          {project.title}
+        </h2>
+        <p className="pt-2">{project.date}</p>
+        <h4 className="pt-6 font-header text-xl font-medium text-black sm:text-2xl">
+          {project.description}
+        </h4>
+
+        <Image
+          src={`/assets/img${project.image}`}
+          className="w-full shadow pt-4"
+          alt="portfolio image"
+          width={552}
+          height={294}
+          priority
+        />
+
+        <p className="font-body leading-relaxed text-grey-20">
+          {project.notes.map((note, index) => (
+            <p key={index} className="pt-6">
+              {note}
+            </p>
           ))}
-        </span>
-      </p>
-      <p className="flex">
-        <span>Repo(s):&nbsp;</span>
-        <span>
-          {sourceUrl.map((str) => (
-            <a key={str} href={str} className="block">
-              {str}
-            </a>
-          ))}
-        </span>
-      </p>
+        </p>
+
+        <div className="grid gap-4 grid-cols-12 pt-4 text-grey-20">
+          <div className="col-span-3">Tech used:</div>
+          <div className="col-span-9">{project.tech.join(", ")}</div>
+          <div className="col-span-3">Live site:</div>
+          <div className="col-span-9 text-black">
+            {url.map((str) => (
+              <a key={str} href={str} className="block">
+                {str}
+              </a>
+            ))}
+          </div>
+          <div className="col-span-3">Code:</div>
+          <div className="col-span-9 text-black">
+            {sourceUrl.map((str) => (
+              <a key={str} href={str} className="block">
+                {str}
+              </a>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
