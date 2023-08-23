0cb6ef178c041a8ebf3df656cef891846435db95
Luke D <luke.warmwater6@gmail.com>
2023-08-23 13:03:24 -0400

    Fixed menu on projects page

--- app/TopMenu.tsx
+++ app/TopMenu.tsx
@@ -1,10 +1,27 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import NavLink from "./navLink";
 
 export default function TopMenu() {
+  const pathname = usePathname();
+  const isHomepage = pathname === "/";
+
+  let containerClassName = "w-full z-50 top-0 py-3 sm:py-5";
+
+  if (isHomepage) {
+    containerClassName += " absolute";
+  } else {
+    containerClassName += " bg-primary";
+  }
+
   return (
-    <div className="w-full z-50 top-0 py-3 sm:py-5  absolute">
+    <div className={containerClassName}>
       <div className="container flex items-center justify-between">
-        <div />
+        <Link href="/" className="text-white uppercase font-semibold">
+          {!isHomepage && "Home"}
+        </Link>
         <div className="block">
           <ul className="flex items-center">
             <NavLink id="about" text="About" />
--- app/navLink.tsx
+++ app/navLink.tsx
@@ -1,21 +1,14 @@
-"use client";
+import Link from "next/link";
 
 export default function NavLink(props: { text: string; id: string }) {
-  const onClick = () => {
-    const element = document.getElementById(props.id);
-    if (element) {
-      element.scrollIntoView();
-    }
-  };
-
   return (
     <li className="group pl-6">
-      <span
-        onClick={onClick}
+      <Link
+        href={"/#" + props.id}
         className="cursor-pointer pt-0.5 font-header font-semibold uppercase text-white"
       >
         {props.text}
-      </span>
+      </Link>
 
       <span className="block h-0.5 w-full bg-transparent group-hover:bg-yellow"></span>
     </li>
